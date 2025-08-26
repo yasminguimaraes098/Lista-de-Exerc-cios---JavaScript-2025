06ad017444de6f7a7d7329438b6903006c250d11
Yasmin Guimarães <yasmin.guimaraes@estudante.iftm.edu.br>
2025-08-26 11:31:55 -0300

    teste git

--- Lista 01/Ex 03/js/script.js
+++ Lista 01/Ex 03/js/script.js
@@ -1,1 +1,7 @@
-prompt
+nome = prompt("Digite seu nome completo:")
+idade = prompt("Agora digite sua idade:")
+
+if (idade >= 18)
+    alert(`${nome}, você POSSUI idade para tirar carteira. Parabéns!`)
+else
+    alert(`${nome}, você ainda NÃO POSSUI idade para tirar carteira. Ainda faltam ${18 - idade} anos.`)
---
+++ Lista 01/Ex 04/js/script.js
@@ -0,0 +1,2 @@
+nota1 = prompt("Digite a nota do 1° bimestre")
+nota2 = prompt("Digite a nota do 2° bimestre")
