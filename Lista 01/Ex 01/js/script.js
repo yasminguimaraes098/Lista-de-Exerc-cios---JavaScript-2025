d148364fae0dca63441488fe795d42a4f0ca4e57
Yasmin Guimarães <yasmin.guimaraes@estudante.iftm.edu.br>
2025-08-26 10:43:04 -0300

    Lista 01 - JS

---
+++ Lista 01/Ex 01/js/script.js
@@ -0,0 +1,5 @@
+nome = prompt ("Digite seu nome:")
+sobrenome = prompt ("Digite seu sobrenone:")
+
+alert(`Seu nome completo é ${nome + " " + sobrenome}: `)
+
---
+++ Lista 01/Ex 02/js/script.js
@@ -0,0 +1,1 @@
+alert ("Precione o botão OK para visualizar o nome da disciplina")
